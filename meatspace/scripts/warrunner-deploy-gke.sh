e8d3e7d045b44d270cb0665b614815749601e90b
harrisonbay <hcybay2@gmail.com>
2026-05-25 17:47:06 -0400

    Pin Warrunner deploy to first-boot hardening images

--- meatspace/scripts/warrunner-deploy-gke.sh
+++ meatspace/scripts/warrunner-deploy-gke.sh
@@ -7,7 +7,7 @@ EGRESS_NAMESPACE="${WARRUNNER_EGRESS_NAMESPACE:-centaur-egress}"
 PROJECT_ID="${GOOGLE_CLOUD_PROJECT:-dapp-455423}"
 LOCATION="us-west1"
 REPOSITORY="warrunner"
-IMAGE_TAG="${WARRUNNER_IMAGE_TAG:-c41aac82}"
+IMAGE_TAG="${WARRUNNER_IMAGE_TAG:-99eb4faf}"
 ENV_FILE="${WARRUNNER_DEPLOY_ENV_FILE:-$HOME/.config/warrunner/deploy.env}"
 RENDER_ONLY=0
 SKIP_IMAGE_CHECK=0
@@ -31,7 +31,7 @@ Options:
   --project <id>            Google Cloud project. Default: dapp-455423
   --location <name>         Artifact Registry location. Default: us-west1
   --repository <name>       Artifact Registry repository. Default: warrunner
-  --image-tag <tag>         Image tag. Default: c41aac82
+  --image-tag <tag>         Image tag. Default: 99eb4faf
   --env-file <path>         Deploy env file. Default: ~/.config/warrunner/deploy.env
   --render-only             Render Helm manifests and verify invariants; do not deploy
   --skip-image-check        Do not verify image tags in Artifact Registry
